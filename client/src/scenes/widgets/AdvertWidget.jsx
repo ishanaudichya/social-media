f582d60364859e63475806325afefbcbcf806c8c
Ishan Audichya <audichyaishan@gmail.com>
2024-08-07 11:39:45 +0530

    small bug fix

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,11 +20,10 @@ const AdvertWidget = () => {
         <Typography color={main}>Ishan Audichya</Typography>
         <Typography color={medium}><a href='https://www.linkedin.com/in/ishan-audichya/' style={{textDecoration: 'none'}}>LinkedIn/ishanaudichya</a></Typography>
       </FlexBetween>
-      <FlexBetween>
-        <Typography color={main}>Shailza Thakur</Typography>
-        <Typography color={medium}><a href='https://www.linkedin.com/in/shailzathakur0707/' style={{textDecoration: 'none'}}>LinkedIn/shailzathakur</a></Typography>
-      </FlexBetween>
-      <Typography color={medium} m="0.5rem 0">
+       <Typography color={medium} m="0.5rem 0">
+     <b>
+       This is only a test demo with a test DB. Other users are hidden for privacy.
+      </b> <br/>
         Make sure to check out our socials and appreciate if u liked our effort
       </Typography>
     </WidgetWrapper>
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 // import { users, posts } from "./data/index.js";
 
-/* CONFIGURATIONS */
+// config
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* FILE STORAGE */
+// multer storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -43,16 +43,15 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-/* ROUTES WITH FILES */
+// file routes
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-/* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-/* MONGOOSE SETUP */
+// mongo setip
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -62,7 +61,7 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME */
+  //  to add manual
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
